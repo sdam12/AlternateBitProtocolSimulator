b58da40483eb4b8affafc66cdd38568345e3a2f9
Saurabh Damani <saurabh.damani12@gmail.com>
2019-08-01 09:39:16 -0400

    Coding Conventions
    Changed Following Files: receivercadmium.hpp,subnetcadmium.hpp and Networkcadmium.hpp
    
    Co-Authored-By: Sagnik Banerjee <nick7695@users.noreply.github.com>

--- include/receiverCadmium.hpp
+++ include/receiverCadmium.hpp
@@ -28,7 +28,7 @@
 using namespace cadmium;
 using namespace std;
 
-//Port definition
+	//Port definition
     struct Receiver_defs{
         struct out : public out_port<Message_t> {
         };
@@ -41,18 +41,18 @@ using namespace std;
         using defs=Receiver_defs; // putting definitions in context
         public:
             //Parameters to be overwriten when instantiating the atomic model
-            TIME   preparationTime;
+            TIME   preparation_time;
             // default constructor
             Receiver() noexcept{
-              preparationTime  = TIME("00:00:10");
-              state.ackNum    = 0;
-              state.sending     = false;
+                preparation_time  = TIME("00:00:10");
+                state.ack_num    = 0;
+                state.sending     = false;
             }
             
             // state definition
             struct state_type{
-              int ackNum;
-              bool sending;
+                int ack_num;
+                bool sending;
             }; 
             state_type state;
             // ports definition
@@ -61,16 +61,18 @@ using namespace std;
 
             // internal transition
             void internal_transition() {
-              state.sending = false; 
+                state.sending = false; 
             }
 
             // external transition
             void external_transition(TIME e, typename make_message_bags<input_ports>::type mbs) { 
-              if(get_messages<typename defs::in>(mbs).size()>1) assert(false && "one message per time uniti");
-              for(const auto &x : get_messages<typename defs::in>(mbs)){
-                state.ackNum = static_cast < int > (x.value);
-                state.sending = true;
-              }  
+                if(get_messages<typename defs::in>(mbs).size()>1) {
+				    assert(false && "one message per time uniti");
+                }
+				for(const auto &x : get_messages<typename defs::in>(mbs)){
+                    state.ack_num = static_cast < int > (x.value);
+                    state.sending = true;
+                }  
                            
             }
 
@@ -82,31 +84,28 @@ using namespace std;
 
             // output function
             typename make_message_bags<output_ports>::type output() const {
-              typename make_message_bags<output_ports>::type bags;
-              Message_t out;              
-              out.value = state.ackNum % 10;
-              get_messages<typename defs::out>(bags).push_back(out);
-                
-              return bags;
-
+                typename make_message_bags<output_ports>::type bags;
+                Message_t out;              
+                out.value = state.ack_num % 10;
+                get_messages<typename defs::out>(bags).push_back(out);
+                return bags;
             }
 
             // time_advance function
             TIME time_advance() const {  
-              TIME next_internal;
-              if (state.sending) {
-                next_internal = preparationTime;
-              }else {
-                next_internal = std::numeric_limits<TIME>::infinity();
-              }    
-              return next_internal;
+                TIME next_internal;
+                if (state.sending) {
+                    next_internal = preparation_time;
+                }
+				else {
+                    next_internal = std::numeric_limits<TIME>::infinity();
+                }    
+                return next_internal;
             }
 
             friend std::ostringstream& operator<<(std::ostringstream& os, const typename Receiver<TIME>::state_type& i) {
-                os << "ackNum: " << i.ackNum; 
-            return os;
+                os << "ack_num: " << i.ack_num; 
+                return os;
             }
-        };     
-  
-
+    };     
 #endif // BOOST_SIMULATION_PDEVS_RECEIVER_HPP

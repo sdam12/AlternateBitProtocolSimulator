ed66c2037f7a622926fb4c37cc725a155743a969
Saurabh Damani <saurabh.damani12@gmail.com>
2019-08-05 12:29:13 -0400

    Changes in Structure name from Message_t to message_t
    Changes requested for Structure name. Name should be change from Message_t to message_t. Since Structure name cannot start with capital letter.
    
    Co-Authored-By: sdam12 <sdam12@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -31,24 +31,24 @@ using namespace std;
 
 
     /***** SETING INPUT PORTS FOR COUPLEDs *****/
-    struct inp_control : public cadmium::in_port<Message_t>{};
-    struct inp_1 : public cadmium::in_port<Message_t>{};
-    struct inp_2 : public cadmium::in_port<Message_t>{};
+    struct inp_control : public cadmium::in_port<message_t>{};
+    struct inp_1 : public cadmium::in_port<message_t>{};
+    struct inp_2 : public cadmium::in_port<message_t>{};
     /***** SETING OUTPUT PORTS FOR COUPLEDs *****/
-    struct outp_ack : public cadmium::out_port<Message_t>{};
-    struct outp_1 : public cadmium::out_port<Message_t>{};
-    struct outp_2 : public cadmium::out_port<Message_t>{};
-    struct outp_pack : public cadmium::out_port<Message_t>{};
+    struct outp_ack : public cadmium::out_port<message_t>{};
+    struct outp_1 : public cadmium::out_port<message_t>{};
+    struct outp_2 : public cadmium::out_port<message_t>{};
+    struct outp_pack : public cadmium::out_port<message_t>{};
     
     
     /********************************************/
     /****** APPLICATION GENERATOR *******************/
     /********************************************/
     template<typename T>
-    class ApplicationGen : public iestream_input<Message_t,T> {
+    class ApplicationGen : public iestream_input<message_t,T> {
         public:
             ApplicationGen() = default;
-            ApplicationGen(const char* file_path) : iestream_input<Message_t,T>
+            ApplicationGen(const char* file_path) : iestream_input<message_t,T>
 		    (file_path) {}
     };
     
@@ -217,7 +217,7 @@ using namespace std;
         };
         cadmium::dynamic::modeling::ICs ics_TOP = {
             cadmium::dynamic::translate::make_IC<iestream_input_defs
-	    	<Message_t>::out,inp_control>("generator_con","ABPSimulator")
+	    	<message_t>::out,inp_control>("generator_con","ABPSimulator")
         };
         std::shared_ptr<cadmium::dynamic::modeling::coupled<TIME>> TOP = 
 	    std::make_shared<cadmium::dynamic::modeling::coupled<TIME>>(
